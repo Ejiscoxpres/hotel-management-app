98bb87f21b9e5a759fcfbc254f0a668c6fc04936
Ejiscoexpress <ejiscoexpress@gmail.com>
2024-01-20 16:01:43 +0100

    PageSearch/Gallery

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,9 +1,16 @@
+import Gallery from "../components/Gallery/Gallery";
 import HeroSection from "../components/HeroSection/HeroSection";
+import PageSearch from "../components/PageSearch/PageSearch";
 
 const Home = () =>{
 return(
   <>
   <HeroSection />
+
+  <PageSearch />
+  {/**Featured Room */}
+  < Gallery />
+  {/**News Letter */}
   </>
 );
   
