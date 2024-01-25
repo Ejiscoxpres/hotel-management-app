a9283932613187df3779b99541f7ee0a9cb06399
Ejiscoexpress <ejiscoexpress@gmail.com>
2024-01-25 11:44:06 +0100

    FeaturedRomm

--- src/app/components/FeaturedRoom/FeaturedRoom.tsx
+++ src/app/components/FeaturedRoom/FeaturedRoom.tsx
@@ -1,6 +1,45 @@
-const FeaturedRoom = () => {
+import { FC } from "react";
+import { Room } from "../../models/room";
+import Image from "next/image";
+
+type Props = {
+  featuredRoom: Room;
+};
+const FeaturedRoom: FC<Props> = props  => {
+
+  const { featuredRoom } = props;
   return (
-    <div>FeaturedRoom</div>
+    <section className='flex md:flex-row flex-col px-4 py-10 items-center gap-12 container mx-auto'>
+      <div className='md:grid gap-8 grid-cols-1'>
+        <div className='rounded-2xl overflow-hidden h-48 mb-4 md:mb-0'>
+        <Image
+            src={featuredRoom.coverImage.url}
+            alt={featuredRoom.name}
+            width={300}
+            height={300}
+            className='img scale-animation'
+          />
+        </div>
+        <div className='grid grid-cols-2 gap-8 h-48'>
+        {featuredRoom.images.splice(1, 2).map(image => (
+            <div key={image._key} className='rounded-2xl overflow-hidden'>
+              <Image
+                src={image.url}
+                alt={image._key}
+                width={300}
+                height={300}
+                className='img scale-animation'
+              />
+            </div>
+          ))}
+
+        </div>
+
+      </div>
+      <div >
+
+      </div>
+      </section>
   )
 }
 
